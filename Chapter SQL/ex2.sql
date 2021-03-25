81051d37d87e12f363b1a6ebc903939d429a1a48
Dimitar Banchev <banchev_dimitar@abv.bg>
2021-03-25 14:44:11 +0100

    made to g

--- Chapter SQL/ex2.sql
+++ Chapter SQL/ex2.sql
@@ -3,4 +3,19 @@
  select * from CAR
 
  --(b)List of chauffeurs
- select Unique PERSON.PNo, PERSON.FirstName, PERSON.Name, Plate, Make, Type from CAR, PERSON
+ select distinct * from PERSON
+ 
+ --(c)List of segments with distance
+ select *, (KmFinish-KmStart)'Distance(km)' from SEGMENT
+
+ --(d)List of segments with distance in miles (1mile = 1.609 km) 
+  select *, ((KmFinish-KmStart)/1.609)'Distance(mile)' from SEGMENT
+
+--(e)List of administrators not living in „Böblingen
+select * from PERSON where PERSON.Residence != 'Böblingen'
+
+--(f)List of cars licensed in Stuttgart
+select * from CAR where CAR.Plate not like 'S%'
+
+--(g)List of segments with a distance between 1000 and 2000 km
+select *, (KmFinish-KmStart)'Distance(km)' from SEGMENT where (KmFinish - KmStart) >= 1000 and (KmFinish - KmStart) <= 2000	
