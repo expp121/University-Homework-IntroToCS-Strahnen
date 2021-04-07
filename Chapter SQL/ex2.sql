0c22c906b79ffd1c4b5fbbdebb60faa83fadd5b1
expp <banchev_dimitar@abv.bg>
2021-04-07 14:34:40 +0200

    Solved to point j

--- Chapter SQL/ex2.sql
+++ Chapter SQL/ex2.sql
@@ -11,11 +11,21 @@
  --(d)List of segments with distance in miles (1mile = 1.609 km) 
   select *, ((KmFinish-KmStart)/1.609)'Distance(mile)' from SEGMENT
 
---(e)List of administrators not living in „Böblingen
-select * from PERSON where PERSON.Residence != 'Böblingen'
+--(e)List of administrators not living in â€žBÃ¶blingen
+select * from PERSON where PERSON.Role = 'A' and PERSON.Residence != 'BÃ¶blingen'
 
 --(f)List of cars licensed in Stuttgart
-select * from CAR where CAR.Plate not like 'S%'
+select * from CAR where CAR.Plate like 'S_%'
 
 --(g)List of segments with a distance between 1000 and 2000 km
 select *, (KmFinish-KmStart)'Distance(km)' from SEGMENT where (KmFinish - KmStart) >= 1000 and (KmFinish - KmStart) <= 2000	
+
+--(h)List of segments traversed by Mercecdes-Benz cars
+select distinct SEGMENT.*, CAR.Make from SEGMENT, CAR where CAR.Make = 'Mercedes-Benz'
+
+
+--(i)Which person (PNo) has never used a car?
+select distinct PERSON.PNo, (RTRIM(FirstName) + ' ' + RTRIM(Name))Name, Role from PERSON where PERSON.PNo not in (select PNo from SEGMENTPERSON)
+
+--(j) List of residences in descending order
+select Residence from PERSON ORDER BY Residence desc
