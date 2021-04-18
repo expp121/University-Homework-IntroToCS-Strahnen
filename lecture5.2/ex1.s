8df11c27348abe42f1d48d18566f72cea6d9fd98
expp <banchev_dimitar@abv.bg>
2021-04-18 14:52:29 +0200

    1

--- lecture5.2/ex1.s
+++ lecture5.2/ex1.s
@@ -1,8 +1,8 @@
 # Write a program which adds following 3 numbers: -600, 4000 and 2000
-	
+
 	.data
 
-A:	.hword	600
+A:	.hword	-600
 B:	.hword	2000
 C:	.hword	4000
 
@@ -11,17 +11,18 @@ C:	.hword	4000
 	
 _start:
 	movia r2, B	# loads the memory address of variable B into R2
-	ldhu r2, (r2)	# gets the value stored at the memory address of B
+	ldhu r2, (r2)	# gets the value stored at the memory address of B (Get unsigned 16 bit value)
 	
 	movia r3, C	# loads the memory address of variable C into R3
-	ldhu r3, (r3)	# gets the value stored at the memory address of C
+	ldhu r3, (r3)	# gets the value stored at the memory address of C (Get unsigned 16 bit value)
 	
 	add r2,r2,r3	# adds r2 and r3 and saves it in r2 -> r2 = r2 + r3 ( r2 = B + C)
 	
 	movia r3, A	# loads the memory address of variable A into R3
-	ldhu r3, (r3)	# gets the value stored at the memory address of A
+	ldh r3, (r3)	# gets the value stored at the memory address of A (Get signed 16 bit value)
 	
-	sub r2, r2, r3	# subtracts r2 and r3, and saves it in r2 -> r2 = r2 - r3 (r2 = (B + C) - A)
+	add r2, r2, r3	# adds r2 and r3, and saves it in r2 -> r2 = r2 + r3 (r2 = (B + C) + (-A))
+	
 	
 
 endloop:
